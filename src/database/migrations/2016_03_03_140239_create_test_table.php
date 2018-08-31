1c79823b9cb5a0dbf3cd1dd7306755a9ebc9f22b
T. Kunze <thku@users.noreply.github.com>
2018-08-31 18:48:32 +0200

    Update 2016_03_03_140239_create_test_table.php

--- src/database/migrations/2016_03_03_140239_create_test_table.php
+++ src/database/migrations/2016_03_03_140239_create_test_table.php
@@ -26,17 +26,17 @@ class CreateTestTable extends Migration
     {
         Schema::create('tests', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->enum('type', ['test', 'project']);
-            $table->integer('repetitions')->unsigned()->default(1);
-            $table->timestamp('repetitions_interval');
-			$table->timestamp('time_limit');
-            $table->boolean('continueable')->default(false);
-            $table->boolean('reporting')->default(true);
-            $table->boolean('caching')->default(true);
-            $table->boolean('active')->default(false);
+            $table->string('name')->nullable();
+            $table->enum('type', ['test', 'project'])->default('project');
+            $table->integer('repetitions')->unsigned()->nullable()->default(1);
+            $table->timestamp('repetitions_interval')->nullable();
+			$table->timestamp('time_limit')->nullable();
+            $table->boolean('continueable')->nullable()->default(false);
+            $table->boolean('reporting')->nullable()->default(true);
+            $table->boolean('caching')->nullable()->default(true);
+            $table->boolean('active')->nullable()->default(false);
             $table->text('assets')->nullable()->default(null);
-            $table->timestamps();
+            $table->nullableTimestamps();
         });
     }
 
